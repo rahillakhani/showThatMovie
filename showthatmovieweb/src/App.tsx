eb79441ffa90e32b120d6c38cdebd7b2607aec86
rahillakhani <lakhani.rj@gmail.com>
2024-04-22 06:53:25 +0530

    web setup

---
+++ showthatmovieweb/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import fetchdata from "fetchdata";
+function App() {
+    console.log("fetchdata",fetchdata);
+  return (
+    <>jhere</>
+  );
+}
+
+export default App;
